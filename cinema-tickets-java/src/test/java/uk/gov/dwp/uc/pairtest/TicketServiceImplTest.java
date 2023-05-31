bd250178cf4a9488104c811f3a6158a649644a59
mozbekler <104729951+mozbekler@users.noreply.github.com>
2023-05-31 21:42:06 +0100

    Unit tests are added

---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,83 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+class TicketServiceImplTest {
+
+    @Mock
+    private TicketPaymentService ticketPaymentService;
+
+    @Mock
+    private SeatReservationService seatReservationService;
+
+    private TicketServiceImpl ticketService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        ticketService = new TicketServiceImpl(ticketPaymentService, seatReservationService);
+    }
+
+    @Test
+    void purchaseTickets_validPurchase_success() throws InvalidPurchaseException {
+        TicketTypeRequest ticketTypeRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
+        TicketTypeRequest[] ticketTypeRequests = {ticketTypeRequest};
+
+        ticketService.purchaseTickets(123L, ticketTypeRequests);
+
+        verify(ticketPaymentService, times(1)).makePayment(123L, 40); // Assuming ticket price for ADULT is 50
+        verify(seatReservationService, times(1)).reserveSeat(123L, 2);
+    }
+
+    @Test
+    void purchaseTickets_invalidAccountId_throwsException() {
+        TicketTypeRequest ticketTypeRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
+        TicketTypeRequest[] ticketTypeRequests = {ticketTypeRequest};
+
+        Assertions.assertThrows(InvalidPurchaseException.class, () -> {
+            ticketService.purchaseTickets(null, ticketTypeRequests);
+        });
+
+        Mockito.verifyNoInteractions(ticketPaymentService);
+        Mockito.verifyNoInteractions(seatReservationService);
+    }
+
+    @Test
+    void purchaseTickets_noAdultTickets_throwsException() {
+        TicketTypeRequest ticketTypeRequest = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2);
+        TicketTypeRequest[] ticketTypeRequests = {ticketTypeRequest};
+
+        Assertions.assertThrows(InvalidPurchaseException.class, () -> {
+            ticketService.purchaseTickets(123L, ticketTypeRequests);
+        });
+
+        Mockito.verifyNoInteractions(ticketPaymentService);
+        Mockito.verifyNoInteractions(seatReservationService);
+    }
+
+    @Test
+    void purchaseTickets_exceedMaxTickets_throwsException() {
+        TicketTypeRequest ticketTypeRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 21);
+        TicketTypeRequest[] ticketTypeRequests = {ticketTypeRequest};
+
+        Assertions.assertThrows(InvalidPurchaseException.class, () -> {
+            ticketService.purchaseTickets(123L, ticketTypeRequests);
+        });
+
+        Mockito.verifyNoInteractions(ticketPaymentService);
+        Mockito.verifyNoInteractions(seatReservationService);
+    }
+}
